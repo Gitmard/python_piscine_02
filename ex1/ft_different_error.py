9a9e800a9a41a514cd4538489aeff81e29b6a696
smenard <smenard@student.42lyon.fr>
2026-03-09 10:37:49 +0100

    fix(ex1): reworked the functions garden_operations and test_error_types to comply with the subject's requirements

--- ex1/ft_different_error.py
+++ ex1/ft_different_error.py
@@ -1,61 +1,55 @@
 #!/usr/bin/python3
 
-def test_value_error():
-    # Imaginary numbers ? Yeah right...
-    return int("i")
-
-
-def test_zero_division_error():
-    # Marcel could do it.
-    return 42 / 0
-
-
-def test_file_not_found_error():
-    # The cake is a lie !
-    open("the cake").close()
-
-
-def test_key_error():
-    # Evidently, there is no spoon
-    return {"not a spoon": "is"}["a spoon"]
-
-
-def test_error_types():
+def garden_operations(error: str) -> None:
     print("\nTesting multiple error together...")
-    try:
-        test_value_error()
-        test_zero_division_error()
-        test_file_not_found_error()
-        test_key_error()
-    except (ValueError, ZeroDivisionError, FileNotFoundError, KeyError):
-        print("Caught an error. but program continues!")
+    if error == "value":
+        # Imaginary numbers ? Yeah right...
+        int("i")
+    elif error == "zero_division":
+        # Marcel could do it.
+        42 / 0
+    elif error == "file_not_found":
+        # The cake is a lie !
+        open("the cake").close()
+    elif error == "key_error":
+        # Evidently, there is no spoon
+        {"not a spoon": "is"}["a spoon"]
+    elif error == "all":
+        # Random bullshit, go !!
+        int("i")
+        42 / 0
+        open("the cake").close()
+        {"not a spoon": "is"}["a spoon"]
 
 
-def garden_operations():
+def test_error_types() -> None:
     print("Testing ValueError...")
     try:
-        test_value_error()
+        garden_operations("value")
     except ValueError as value_error:
         print(f"Caught value error: {value_error}")
     print("\nTesting ZeroDivisionError...")
     try:
-        test_zero_division_error()
+        garden_operations("zero_division")
     except ZeroDivisionError as zero_division_error:
         print(f"Caught ZeroDivisionError: {zero_division_error}")
     print("\nTesting FileNotFoundError...")
     try:
-        test_file_not_found_error()
+        garden_operations("file_not_found")
     except FileNotFoundError as file_not_found_error:
         print(f"Caught FileNotFoundError: {file_not_found_error}")
     print("\nTesting KeyError...")
     try:
-        test_key_error()
+        garden_operations("key_error")
     except KeyError as key_error:
         print(f"Caught KeyError: {key_error}")
+    try:
+        garden_operations("all")
+    except (ValueError, ZeroDivisionError, FileNotFoundError, KeyError):
+        print("Caught an error, but program continues !")
 
 
 if __name__ == "__main__":
     print("=== Garden Error Types Demo ===\n")
-    garden_operations()
     test_error_types()
     print("\nAll error types tested successfully!")
